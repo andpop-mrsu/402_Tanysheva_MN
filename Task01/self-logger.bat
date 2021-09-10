4099d212f20ec69c67abffd3e1ccc109a64a27f1
Maria-brash <90049947+Maria-brash@users.noreply.github.com>
2021-09-10 17:50:25 +0200

    Add files via upload

---
+++ Task01/self-logger.bat
@@ -0,0 +1,19 @@
+@echo off
+chcp 65001>nul
+
+where sqlite3>nul 2>nul
+if %ERRORLEVEL% NEQ 0 ( echo Команда sqlite3 не найдена! & pause & exit ) 
+echo create table if not exists logs(User varchar(10), Date text default current_timestamp); | sqlite3 bd_1.db
+echo insert into logs values('%USERNAME%', datetime('now', 'localtime')); | sqlite3 bd_1.db
+
+echo Имя программы: %~nx0
+echo|<nul set /p="Количество запусков: "
+echo select count(*) from logs; | sqlite3 bd_1.db
+echo|<nul set /p="Первый запуск: "
+echo select Date from logs order by Date asc limit 1; | sqlite3 bd_1.db
+
+echo.
+echo select * from logs; | sqlite3 -table bd_1.db
+echo. 
+
+pause
